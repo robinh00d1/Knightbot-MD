3183211f08cb46c9643c32c8943c2ff7fd8cadd0
robinh00d1 <waleedwaheed2426@gmail.com>
2025-05-27 10:06:07 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "khokhar",
+  botOwner: '⇀𝄟≛⃝ 𝙎𝘼𝙇𝘼𝙍 𝄟≛⃝🐼', // Your name
+  ownerNumber: '923097621856', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
